94d72994715e4b99acea22efe1cce39b0c1d1d8d
JaretWright <hkkA223>
2018-02-21 12:49:47 -0500

    Added view and controller classes

---
+++ src/w18comp1008s3feb21/CardViewController.java
@@ -0,0 +1,32 @@
+package w18comp1008s3feb21;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
+
+/**
+ * FXML Controller class
+ *
+ * @author JWright
+ */
+public class CardViewController implements Initializable
+{
+    @FXML    private Label faceNameLabel;
+    @FXML    private Label suitLabel;
+    @FXML    private Label valueLabel;
+             private Card activeCard;
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb)
+    {
+        //create a deck of cards each time the view is launched
+        DeckOfCards deck = new DeckOfCards();
+        
+    }    
+    
+}
